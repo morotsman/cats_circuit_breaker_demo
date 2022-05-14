4ddce71a8878125d8b786e0dabe74f6bfac81d66
Niklas Leopold <leopold.niklas@gmail.com>
2022-05-14 11:20:52 +0200

    circuit breaker

--- src/main/scala/presentation/Main.scala
+++ src/main/scala/presentation/Main.scala
@@ -2,72 +2,27 @@ package com.github.morotsman
 package presentation
 
 import cats.effect._
-import com.github.morotsman.presentation.demo.CircuitBreakerState.CircuitBreakerState
-import com.github.morotsman.presentation.demo.{CircuitBreakerState, DemoProgram, MayhemState, SourceOfMayhem, Statistics, StatisticsInfo}
-import com.github.morotsman.presentation.slides.demo_slide.{CircuitBreakerDemo, CircuitBreakerDemoState, CircuitBreakerConfiguration}
+import com.github.morotsman.presentation.slides.demo_slide.{CircuitBreakerSlide, CircuitBreakerSlideState}
 import com.github.morotsman.presentation.slides.{Agenda, Start}
 import com.github.morotsman.presentation.tools.{Input, NConsole, Presentation, PresentationState}
-import io.chrisdavenport.circuit.{Backoff, CircuitBreaker}
-
-import scala.concurrent.duration._
 
 object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
     (for {
       console <- NConsole.make[IO]()
-      (sourceOfMayhem, statistics) <- setupDemoApp()
-      circuitBreakerDemoSlide <- Ref[IO].of(CircuitBreakerDemoState.initial[IO]()).map(CircuitBreakerDemo[IO](
-        console = console,
-        demoProgramFactory = createDemoApp,
-        sourceOfMayhem = sourceOfMayhem,
-        statistics = statistics,
-        _
-      ))
+      slide <- Ref[IO].of(CircuitBreakerSlideState.make[IO]()).map(CircuitBreakerSlide[IO](console, _))
       presentation <- Ref[IO]
         .of(PresentationState.initialState(List(
           Start[IO](console),
           Agenda[IO](console),
-          circuitBreakerDemoSlide
+          slide
         )))
         .flatMap(Presentation.make[IO](console, _))
       _ <- presentation.start().start
       _ <- IO(handleInput(console, presentation)).flatten.foreverM
     } yield ()).map(_ => ExitCode.Success)
   }
-
-  // TODO, move this to CircuitBreakerDemo
-  private def createDemoApp(
-                             configuration: CircuitBreakerConfiguration,
-                             sourceOfMayhem: SourceOfMayhem[IO],
-                             statistics: Statistics[IO]
-                           ): IO[DemoProgram[IO]] = {
-    CircuitBreaker.of[IO](
-      maxFailures = configuration.maxFailures,
-      resetTimeout = configuration.resetTimeout,
-      backoff = Backoff.exponential,
-      maxResetTimeout = configuration.maxResetTimeout,
-      onOpen = statistics.circuitBreakerStateChange(CircuitBreakerState.OPEN),
-      onClosed = statistics.circuitBreakerStateChange(CircuitBreakerState.CLOSED),
-      onRejected = IO.unit,
-      onHalfOpen = statistics.circuitBreakerStateChange(CircuitBreakerState.HALF_OPEN)
-    ).map { circuitBreaker =>
-      DemoProgram[IO](
-        sourceOfMayhem = sourceOfMayhem,
-        circuitBreaker = circuitBreaker,
-        statistics = statistics
-      )
-    }
-  }
-
-  private def setupDemoApp() = for {
-    sourceOfMayhem <- Ref[IO].of(MayhemState(
-      isFailing = false,
-      successLatency = 30.millis,
-      requestTimeout = 5.seconds
-    )).map(SourceOfMayhem.make[IO])
-    statistics <- Ref[IO].of(StatisticsInfo.make()).map(Statistics.make[IO])
-  } yield (sourceOfMayhem, statistics)
 
   def handleInput(c: NConsole[IO], presentation: Presentation[IO]): IO[Input] = for {
     input <- c.read()
---
+++ src/main/scala/presentation/slides/demo_slide/CircuitBreakerSlide.scala
@@ -0,0 +1,80 @@
+package com.github.morotsman
+package presentation.slides.demo_slide
+
+import presentation.tools.{Input, NConsole, Slide}
+
+import cats.implicits._
+import cats.MonadError
+import cats.effect.{Ref, Temporal}
+import com.github.morotsman.presentation.demo.{CircuitBreakerState, DemoProgram, MayhemState, SourceOfMayhem, Statistics, StatisticsInfo}
+import io.chrisdavenport.circuit.{Backoff, CircuitBreaker}
+
+import scala.concurrent.duration.DurationInt
+
+final case class CircuitBreakerSlideState[F[_]]
+(
+  slide: Option[CircuitBreakerDemo[F]]
+)
+
+object CircuitBreakerSlideState {
+  def make[F[_]](): CircuitBreakerSlideState[F] = CircuitBreakerSlideState[F](
+    slide = None
+  )
+}
+
+final case class CircuitBreakerSlide[F[_] : Temporal : MonadError[*[_], Throwable]]
+(
+  console: NConsole[F],
+  state: Ref[F, CircuitBreakerSlideState[F]]
+) extends Slide[F] {
+
+  override def start(): F[Unit] = for {
+    sourceOfMayhem <- Ref[F].of(MayhemState(
+      isFailing = false,
+      successLatency = 30.millis,
+      requestTimeout = 5.seconds
+    )).map(SourceOfMayhem.make[F])
+    statistics <- Ref[F].of(StatisticsInfo.make()).map(Statistics.make[F])
+    configuration = CircuitBreakerConfiguration.make()
+    demoProgram <- CircuitBreaker.of[F](
+      maxFailures = configuration.maxFailures,
+      resetTimeout = configuration.resetTimeout,
+      backoff = Backoff.exponential,
+      maxResetTimeout = configuration.maxResetTimeout,
+      onOpen = statistics.circuitBreakerStateChange(CircuitBreakerState.OPEN),
+      onClosed = statistics.circuitBreakerStateChange(CircuitBreakerState.CLOSED),
+      onRejected = MonadError[F, Throwable].unit,
+      onHalfOpen = statistics.circuitBreakerStateChange(CircuitBreakerState.HALF_OPEN)
+    ).map { circuitBreaker =>
+      DemoProgram[F](
+        sourceOfMayhem = sourceOfMayhem,
+        circuitBreaker = circuitBreaker,
+        statistics = statistics
+      )
+    }
+    circuitBreakerDemoSlide <- Ref[F]
+      .of(CircuitBreakerDemoState.initial[F]())
+      .map(CircuitBreakerDemo[F](
+        console = console,
+        demoProgramFactory = (a, b, c) => MonadError[F, Throwable].pure(demoProgram),
+        sourceOfMayhem = sourceOfMayhem,
+        statistics = statistics,
+        _
+      ))
+    _ <- state.modify(s => (s.copy(
+      slide = Option(circuitBreakerDemoSlide)
+    ), s))
+    _ <- circuitBreakerDemoSlide.start()
+  } yield ()
+
+  override def userInput(input: Input): F[Unit] = for {
+    s <- state.get
+    _ <- s.slide.traverse(_.userInput(input))
+  } yield ()
+
+
+  override def exit(): F[Unit] = for {
+    s <- state.get
+    _ <- s.slide.traverse(_.exit())
+  } yield ()
+}
