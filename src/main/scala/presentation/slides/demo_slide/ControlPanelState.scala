f93aa8efb6c2c511cea573d363e59088a14d5052
Niklas Leopold <leopold.niklas@gmail.com>
2022-05-18 14:45:57 +0200

    circuit breaker

--- src/main/scala/presentation/slides/demo_slide/ControlPanelState.scala
+++ src/main/scala/presentation/slides/demo_slide/ControlPanelState.scala
@@ -1,19 +1,7 @@
 package com.github.morotsman
 package presentation.slides.demo_slide
 
-import cats.effect.Fiber
-import presentation.demo.DemoProgram
 import presentation.tools.Input
-
-final case class DemoConfiguration(
-                                    delayBetweenCallToSourceOfMayhemInNanos: Int
-                                  )
-
-object DemoConfiguration {
-  def make(): DemoConfiguration = DemoConfiguration(
-    delayBetweenCallToSourceOfMayhemInNanos = 1000 * 1000 * 1000
-  )
-}
 
 final case class ControlPanelState[F[_]]
 (
