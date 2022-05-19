a5de80fc6b46ee32f8261e733aeb85038dd1f083
Niklas Leopold <leopold.niklas@gmail.com>
2022-05-19 07:25:01 +0200

    circuit breaker

--- src/main/scala/presentation/Main.scala
+++ src/main/scala/presentation/Main.scala
@@ -2,16 +2,18 @@ package com.github.morotsman
 package presentation
 
 import cats.effect._
-import presentation.slides.demo_slide.{CircuitBreakerSlide, CircuitBreakerSlideState}
+import presentation.slides.demo_slide.{CircuitBreakerSlide, ControlPanel, ControlPanelState, DemoProgramExecutor, DemoProgramExecutorState}
 import presentation.slides.{Agenda, Start}
 import presentation.tools.{NConsole, Presentation, PresentationState}
+import presentation.demo.{MayhemState, SourceOfMayhem, Statistics, StatisticsState}
+import presentation.slides.demo_slide.animations.{Animator, AnimatorState}
 
 object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
     (for {
       console <- NConsole.make[IO]()
-      circuitBreakerSlide <- Ref[IO].of(CircuitBreakerSlideState.make[IO]()).map(CircuitBreakerSlide[IO](console, _))
+      circuitBreakerSlide <- createCircuitBreakerSlide(console)
       presentation <- Ref[IO]
         .of(PresentationState.make(List(
           Start[IO](console),
@@ -23,4 +25,29 @@ object Main extends IOApp {
     } yield ()).map(_ => ExitCode.Success)
   }
 
+  private def createCircuitBreakerSlide(console: NConsole[IO]): IO[CircuitBreakerSlide[IO]] = for {
+    sourceOfMayhem <- Ref[IO].of(MayhemState.make()).map(SourceOfMayhem.make[IO])
+    statistics <- Ref[IO].of(StatisticsState.make()).map(Statistics.make[IO])
+    demoProgramExecutor <- Ref[IO].of(DemoProgramExecutorState.make[IO]()).flatMap(state => DemoProgramExecutor.make(
+      state = state,
+      sourceOfMayhem = sourceOfMayhem,
+      statistics = statistics
+    ))
+    controlPanel <- Ref[IO].of(ControlPanelState.make[IO]()).map(state => ControlPanel.make[IO](
+      state = state,
+      sourceOfMayhem = sourceOfMayhem,
+      demoProgramExecutor = demoProgramExecutor
+    ))
+    animator <- Ref[IO].of(AnimatorState.make()).flatMap(state =>
+      Animator.make[IO](state, controlPanel, statistics, sourceOfMayhem, demoProgramExecutor, console)
+    )
+    circuitBreakerSlide = CircuitBreakerSlide[IO](
+      sourceOfMayhem,
+      statistics,
+      demoProgramExecutor,
+      controlPanel,
+      animator,
+      console
+    )
+  } yield circuitBreakerSlide
 }
--- src/main/scala/presentation/slides/demo_slide/CircuitBreakerSlide.scala
+++ src/main/scala/presentation/slides/demo_slide/CircuitBreakerSlide.scala
@@ -5,58 +5,31 @@ import presentation.tools.{Input, NConsole, Slide}
 
 import cats.implicits._
 import cats.effect.implicits._
-import cats.effect.{Ref, Temporal}
-import presentation.demo.{MayhemState, SourceOfMayhem, Statistics, StatisticsState}
-import presentation.slides.demo_slide.animations.{Animator, AnimatorState}
-
-final case class CircuitBreakerSlideState[F[_]]
-(
-  controlPanel: Option[ControlPanel[F]],
-)
-
-object CircuitBreakerSlideState {
-  def make[F[_]](): CircuitBreakerSlideState[F] = CircuitBreakerSlideState[F](
-    controlPanel = None,
-  )
-}
+import cats.effect.Temporal
+import presentation.demo.{SourceOfMayhem, Statistics}
+import presentation.slides.demo_slide.animations.Animator
 
 final case class CircuitBreakerSlide[F[_] : Temporal]
 (
-  console: NConsole[F],
-  state: Ref[F, CircuitBreakerSlideState[F]]
+  sourceOfMayhem: SourceOfMayhem[F],
+  statistics: Statistics[F],
+  demoProgramExecutor: DemoProgramExecutor[F],
+  controlPanel: ControlPanel[F],
+  animator: Animator[F],
+  console: NConsole[F]
 ) extends Slide[F] {
 
-  override def show(): F[Unit] = for {
-    sourceOfMayhem <- Ref[F].of(MayhemState.make()).map(SourceOfMayhem.make[F])
-    statistics <- Ref[F].of(StatisticsState.make()).map(Statistics.make[F])
-    demoProgramExecutor <- Ref[F].of(DemoProgramExecutorState.make[F]()).flatMap(state => DemoProgramExecutor.make(
-      state = state,
-      sourceOfMayhem = sourceOfMayhem,
-      statistics = statistics
-    ))
-    controlPanel <- Ref[F].of(ControlPanelState.make[F]()).map(state => ControlPanel.make[F](
-      state = state,
-      sourceOfMayhem = sourceOfMayhem,
-      demoProgramExecutor = demoProgramExecutor
-    ))
-    animator <- Ref[F].of(AnimatorState.make()).flatMap(state =>
-      Animator.make[F](state, controlPanel, statistics, sourceOfMayhem, demoProgramExecutor, console)
-    )
-    _ <- state.modify(s => (s.copy(
-      controlPanel = Option(controlPanel)
-    ), s))
-    _ <- (
+  override def show(): F[Unit] =
+    (
       statistics.aggregate(),
       demoProgramExecutor.execute(),
       animator.animate()
-    ).parTupled.background.use { _ =>
+      )
+      .parTupled.background.use { _ =>
       demoProgramExecutor.execute()
     }
-  } yield ()
 
-  override def userInput(input: Input): F[Unit] = for {
-    s <- state.get
-    _ <- s.controlPanel.traverse(_.userInput(input))
-  } yield ()
+  override def userInput(input: Input): F[Unit] =
+    controlPanel.userInput(input)
 
 }
