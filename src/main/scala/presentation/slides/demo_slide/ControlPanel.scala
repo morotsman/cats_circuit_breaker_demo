7d06b199d9ad33ae644e798713f195322180d526
Niklas Leopold <leopold.niklas@gmail.com>
2022-05-21 08:39:33 +0200

    circuit breaker

--- src/main/scala/presentation/slides/demo_slide/ControlPanel.scala
+++ src/main/scala/presentation/slides/demo_slide/ControlPanel.scala
@@ -35,79 +35,78 @@ object ControlPanel {
   ): ControlPanel[F] = new ControlPanel[F] {
     override def getState(): F[ControlPanelState[F]] = state.get
 
-    override def userInput(input: Input): F[Unit] = for {
-      _ <- input match {
-        case Character(c) if c == 'f' =>
-          sourceOfMayhem.toggleFailure() >> statistics.currentInput(input)
-        case Character(c) if c == 'n' =>
-          state.modify(s => (s.copy(
-            previousInput = Option(input)
-          ), s)) >> statistics.currentInput(input)
-        case Character(c) if c == 'l' =>
-          state.modify(s => (s.copy(
-            previousInput = Option(input)
-          ), s)) >> statistics.currentInput(input)
-        case Character(c) if c == 't' =>
-          state.modify(s => (s.copy(
-            previousInput = Option(input)
-          ), s)) >> statistics.currentInput(input)
-        case Character(c) if c == 'a' =>
-          state.modify(s => (s.copy(
-            previousInput = Option(input)
-          ), s)) >> statistics.currentInput(input)
-        case Character(c) if c == 'r' =>
-          state.modify(s => (s.copy(
-            previousInput = Option(input)
-          ), s)) >> statistics.currentInput(input)
-        case Character(c) if c == 'm' =>
-          state.modify(s => (s.copy(
-            previousInput = Option(input)
-          ), s)) >> statistics.currentInput(input)
-        case Character(c) if c == '+' =>
-          for {
-            s <- state.get
-            _ <- s.previousInput.traverse {
-              case Character(c) if c == 'n' =>
-                demoProgramExecutor.decreaseDelayBetweenCallsToSourceOfMayhem()
-              case Character(c) if c == 'l' =>
-                sourceOfMayhem.increaseSuccessLatency()
-              case Character(c) if c == 't' =>
-                sourceOfMayhem.increaseRequestTimeout()
-              case Character(c) if c == 'a' =>
-                demoProgramExecutor.increaseMaxFailures()
-              case Character(c) if c == 'r' =>
-                demoProgramExecutor.increaseResetTimeout()
-              case Character(c) if c == 'm' =>
-                demoProgramExecutor.increaseMaxResetTimeout()
-              case _ =>
-                Monad[F].unit
-            }
-          } yield ()
-        case Character(c) if c == '-' =>
-          for {
-            s <- state.get
-            _ <- s.previousInput.traverse {
-              case Character(c) if c == 'n' =>
-                demoProgramExecutor.increaseDelayBetweenCallsToSourceOfMayhem()
-              case Character(c) if c == 'l' =>
-                sourceOfMayhem.decreaseSuccessLatency()
-              case Character(c) if c == 't' =>
-                sourceOfMayhem.decreaseRequestTimeout()
-              case Character(c) if c == 'a' =>
-                demoProgramExecutor.decreaseMaxFailures()
-              case Character(c) if c == 'r' =>
-                demoProgramExecutor.decreaseResetTimeout()
-              case Character(c) if c == 'm' =>
-                demoProgramExecutor.decreaseMaxResetTimeout()
-              case _ =>
-                Monad[F].unit
-            }
-          } yield ()
+    override def userInput(input: Input): F[Unit] = input match {
+      case Character(c) if c == 'f' =>
+        sourceOfMayhem.toggleFailure() >> statistics.currentInput(input)
+      case Character(c) if c == 'n' =>
+        state.modify(s => (s.copy(
+          previousInput = Option(input)
+        ), s)) >> statistics.currentInput(input)
+      case Character(c) if c == 'l' =>
+        state.modify(s => (s.copy(
+          previousInput = Option(input)
+        ), s)) >> statistics.currentInput(input)
+      case Character(c) if c == 't' =>
+        state.modify(s => (s.copy(
+          previousInput = Option(input)
+        ), s)) >> statistics.currentInput(input)
+      case Character(c) if c == 'a' =>
+        state.modify(s => (s.copy(
+          previousInput = Option(input)
+        ), s)) >> statistics.currentInput(input)
+      case Character(c) if c == 'r' =>
+        state.modify(s => (s.copy(
+          previousInput = Option(input)
+        ), s)) >> statistics.currentInput(input)
+      case Character(c) if c == 'm' =>
+        state.modify(s => (s.copy(
+          previousInput = Option(input)
+        ), s)) >> statistics.currentInput(input)
+      case Character(c) if c == '+' =>
+        for {
+          s <- state.get
+          _ <- s.previousInput.traverse {
+            case Character(c) if c == 'n' =>
+              demoProgramExecutor.decreaseDelayBetweenCallsToSourceOfMayhem()
+            case Character(c) if c == 'l' =>
+              sourceOfMayhem.increaseSuccessLatency()
+            case Character(c) if c == 't' =>
+              sourceOfMayhem.increaseRequestTimeout()
+            case Character(c) if c == 'a' =>
+              demoProgramExecutor.increaseMaxFailures()
+            case Character(c) if c == 'r' =>
+              demoProgramExecutor.increaseResetTimeout()
+            case Character(c) if c == 'm' =>
+              demoProgramExecutor.increaseMaxResetTimeout()
+            case _ =>
+              Monad[F].unit
+          }
+        } yield ()
+      case Character(c) if c == '-' =>
+        for {
+          s <- state.get
+          _ <- s.previousInput.traverse {
+            case Character(c) if c == 'n' =>
+              demoProgramExecutor.increaseDelayBetweenCallsToSourceOfMayhem()
+            case Character(c) if c == 'l' =>
+              sourceOfMayhem.decreaseSuccessLatency()
+            case Character(c) if c == 't' =>
+              sourceOfMayhem.decreaseRequestTimeout()
+            case Character(c) if c == 'a' =>
+              demoProgramExecutor.decreaseMaxFailures()
+            case Character(c) if c == 'r' =>
+              demoProgramExecutor.decreaseResetTimeout()
+            case Character(c) if c == 'm' =>
+              demoProgramExecutor.decreaseMaxResetTimeout()
+            case _ =>
+              Monad[F].unit
+          }
+        } yield ()
 
-        case _ =>
-          Monad[F].unit
-      }
-    } yield ()
+      case _ =>
+        Monad[F].unit
+    }
+
 
   }
 }
