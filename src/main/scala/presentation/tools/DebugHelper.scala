c3eedb53076eb761cee90d21984f5ee2f4f7510e
Niklas Leopold <leopold.niklas@gmail.com>
2022-05-21 10:36:13 +0200

    circuit breaker

---
+++ src/main/scala/presentation/tools/DebugHelper.scala
@@ -0,0 +1,18 @@
+package com.github.morotsman
+package presentation.tools
+
+import cats.implicits._
+import cats.{FlatMap}
+
+
+object debug {
+  /** Extension methods for an effect of type `F[A]`. */ implicit class DebugHelper[F[_] : FlatMap, A](fa: F[A]) {
+    /** Print to the console the value of the effect
+     * along with the thread it was computed on. */
+    def debug: F[A] =
+      for {
+        a <- fa
+        tn = Thread.currentThread.getName
+        _ = println(s"[$tn}] $a")
+      } yield a }
+}
--- src/main/scala/presentation/tools/Presentation.scala
+++ src/main/scala/presentation/tools/Presentation.scala
@@ -5,6 +5,7 @@ import cats._
 import cats.effect._
 import cats.effect.implicits._
 import cats.implicits._
+import presentation.tools.debug.DebugHelper
 
 trait Presentation[F[_]] {
   def start(): F[Unit]
